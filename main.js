91374fa6267f5d423f33e1ac1c0a6db7a817134a
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 08:59:07 -0500

    JAVA_04

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-let cont=1;
+let dama= 0;
+let caballero= 7;
 
-while (cont<=5){
-    let nom= prompt("Escribir el nombre del estudiante:");
-    let ape= prompt("Escribir el apellido del estudiante:");
-    let edad= Number(prompt("Escribir la edad del estudiante:"));
-
-    let nota1= Number(prompt("Escribe la primera nota del estudiante:"));
-    let nota2= Number(prompt("Escribe la segunda nota del estudiante:"));
-
-    let prome= (nota1+nota2)/2;
-    alert("El estudiante" +nom+ape+ "su edad es de" +edad+ "tiene una nota promedio de" +prome);
-    cont+=1;
+while (dama+caballero<=42){
+    dama+=1;
+    caballero+=1;
 }
+alert("Las dmaas que habia en el baile eran:" +dama);
