d804e374aa41c873cd689f26b934b0e558673b8f
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 09:59:58 -0500

    JAVA_06

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-let noTieneDere= 0;
-let cont= 0;
+let segun= 0;
+let avion= 22222;
+let acelavion= 20;
+let acelmisil= 10;
 
-while (cont<=40){
-    let nota1= Number(prompt("Digitar la primera nota:"));
-    let nota2= Number(prompt("Digitar la segunda nota:"));
-    let nota3= Number(prompt("Digitar la tercera nota:"));
-    let nota4= Number(prompt("Digitar la cuarta nota:"));
-    let nota5= Number(prompt("Digitar la quinta nota:"));
+while (acelavion-acelmisil<=10000){
+    acelavion+=20+acelavion;
+    acelmisil+=10+acelmisil;
+    segun+=1;
 
-    let pro= (nota1+nota2+nota3+nota4+nota5)/5;
-    cont +=1;
-
-    if (pro<=3){
-        noTieneDere+=1;
-    }
-    alert("La cantidad de alumnos que no tienen derecho al examen de nivelacion son:" +noTieneDere);
+    alert("El segundo" +segun);
+    alert("La distancia del avion es de:" +acelavion);
+    alert("La distancia del misil es de:" +acelmisil);
 }
+alert("Programa finalizado")
