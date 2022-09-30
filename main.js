517d1f78ad550aa9b3c8e585574148bda1822237
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 10:11:30 -0500

    JAVA_07

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let segun= 0;
-let avion= 22222;
-let acelavion= 20;
-let acelmisil= 10;
+let numtraba= Number(prompt("Ingresar el numero de trabajadores:"));
+let cont= 1;
+let comi= 0;
 
-while (acelavion-acelmisil<=10000){
-    acelavion+=20+acelavion;
-    acelmisil+=10+acelmisil;
-    segun+=1;
+while(cont<=numtraba){
+    let sueldo= Number(prompt("Ingresar el sueldo del trabajador"));
+    let vent1= Number(prompt("Ingresar la primera venta:"));
+    let vent2= Number(prompt("Ingresar la segunda venta:"));
+    let vent3= Number(prompt("Ingresar la tercera venta:"));
 
-    alert("El segundo" +segun);
-    alert("La distancia del avion es de:" +acelavion);
-    alert("La distancia del misil es de:" +acelmisil);
+    comi= (vent1+vent2+vent3)*0.10;
+
+    alert("El trabajador tiene un sueldo mensual de" +sueldo+ "con una comision mensual de" +comi);
+    alert("El sueldo total mas la comision es de" +(sueldo+comi));
 }
-alert("Programa finalizado")
