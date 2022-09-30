4c1c2c909c25ce8634b4f5e6ee4aff86f2e27b40
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 09:11:01 -0500

    JAVA_05

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let dama= 0;
-let caballero= 7;
+let noTieneDere= 0;
+let cont= 0;
 
-while (dama+caballero<=42){
-    dama+=1;
-    caballero+=1;
+while (cont<=40){
+    let nota1= Number(prompt("Digitar la primera nota:"));
+    let nota2= Number(prompt("Digitar la segunda nota:"));
+    let nota3= Number(prompt("Digitar la tercera nota:"));
+    let nota4= Number(prompt("Digitar la cuarta nota:"));
+    let nota5= Number(prompt("Digitar la quinta nota:"));
+
+    let pro= (nota1+nota2+nota3+nota4+nota5)/5;
+    cont +=1;
+
+    if (pro<=3){
+        noTieneDere+=1;
+    }
+    alert("La cantidad de alumnos que no tienen derecho al examen de nivelacion son:" +noTieneDere);
 }
-alert("Las dmaas que habia en el baile eran:" +dama);
